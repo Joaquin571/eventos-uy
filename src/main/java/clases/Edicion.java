8945f8aa769c09ea0864d245e9a52e1b021fec94
Joaquin571 <eljoaco211@gmail.com>
2026-08-29 20:47:40 -0300

    agrega entity edicion, persistence xml

--- src/main/java/clases/Edicion.java
+++ src/main/java/clases/Edicion.java
@@ -1,9 +1,15 @@
 package clases;
 
-import java.time.LocalDate;
+import jakarta.persistence.*;
 
+import java.time.LocalDate;
+import java.util.Collection;
+
+
+@Entity
 public class Edicion {
 
+    @Id
     private String idNombre;
     private String sigla;
     private LocalDate fechaInicio;
@@ -12,6 +18,25 @@ public class Edicion {
     private String ciudad;
     private String pais;
 
+    @ManyToOne
+    @JoinColumn(name="ORG_NICKNAME")
+    private Organizador organizador;
+
+    @ManyToOne
+    @JoinColumn(name="EVE_NOMBRE")
+    private Evento evento;
+
+    @OneToMany(mappedBy = "edicion")
+    private Collection<Patrocinio> patrocinios;
+
+    @OneToMany(mappedBy = "edicion")
+    private Collection<TipoRegistro> tipoRegistros;
+
+    @OneToMany(mappedBy = "edicion")
+    private Collection<Registro> registros;
+
+
+    protected Edicion() {}
     public Edicion(
             String idNombre,
             String sigla,
